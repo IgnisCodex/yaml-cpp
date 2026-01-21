d86932ed40f216c4a6991e4bcfcb362076db0fd8
Ignis Codex <90404350+IgnisCodex@users.noreply.github.com>
2026-01-21 00:26:26 +0000

    Update premake5.lua.

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 project "yaml-cpp"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
+	cppdialect "C++20"
 	staticruntime "off"
-	targetdir ("%{wks.location}/build/bin/" .. OUTPUT_DIR .. "/%{prj.name}")
-	objdir ("%{wks.location}/build/int/" .. OUTPUT_DIR .. "/%{prj.name}")
+	targetdir (BIN_DIR .. "/%{prj.name}")
+    objdir (OBJ_DIR .. "/%{prj.name}")
 
 	files {
 		"src/**.h",
@@ -17,25 +17,3 @@ project "yaml-cpp"
 	}
 
 	defines { "YAML_CPP_STATIC_DEFINE" }
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines {
-			"IG_PLATFORM_WINDOWS",
-		}
-
-	filter "configurations:Debug"
-		defines "IG_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "IG_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "IG_DIST"
-		runtime "Release"
-		optimize "on"
